5e8269a5736e6ba331f50dadbf5c468513819be6
Kn0rk3 <georg.leuschel@gmail.com>
2025-01-19 22:01:55 +0100

    changed config

--- src/config/config.js
+++ src/config/config.js
@@ -1,13 +1,13 @@
 const config = {
-    github: {
-      url:  import.meta.env.SWAVE_VITE_GITHUB_URL || import.meta.env.VITE_GITHUB_URL || 'https://github.com'
-    },
-    coffee: {
-      url: import.meta.env.SWAVE_VITE_COFFEE_URL || import.meta.env.VITE_COFFEE_URL || 'https://buymeacoffee.com'
-    },
-    app: {
-      title: import.meta.env.SWAVE_VITE_APP_TITLE || import.meta.env.VITE_APP_TITLE || 'Download My Memories'
-    }
-  };
+  github: {
+    url: import.meta.env.VITE_GITHUB_URL || 'https://github.com'
+  },
+  coffee: {
+    url: import.meta.env.VITE_COFFEE_URL || 'https://buymeacoffee.com'
+  },
+  app: {
+    title: import.meta.env.VITE_APP_TITLE || 'Download My Memories'
+  }
+};
   
   export default config;
