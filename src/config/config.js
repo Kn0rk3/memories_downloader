80d38d40308cbfc25111538ea83932ddeeb2202e
Kn0rk3 <georg.leuschel@gmail.com>
2025-01-19 21:32:35 +0100

    changed env variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 const config = {
     github: {
-      url:process.env.VITE_GITHUB_URL || import.meta.env.VITE_GITHUB_URL || 'https://github.com'
+      url:  import.meta.env.SWAVE_VITE_GITHUB_URL || import.meta.env.VITE_GITHUB_URL || 'https://github.com'
     },
     coffee: {
-      url: process.env.VITE_COFFEE_URL || import.meta.env.VITE_COFFEE_URL || 'https://buymeacoffee.com'
+      url: import.meta.env.SWAVE_VITE_COFFEE_URL || import.meta.env.VITE_COFFEE_URL || 'https://buymeacoffee.com'
     },
     app: {
-      title: process.env.VITE_APP_TITLE || import.meta.env.VITE_APP_TITLE || 'Download My Memories'
+      title: import.meta.env.SWAVE_VITE_APP_TITLE || import.meta.env.VITE_APP_TITLE || 'Download My Memories'
     }
   };
   
