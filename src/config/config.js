a926ef3eeb4cfd417969cef3424d06f00234112c
Kn0rk3 <georg.leuschel@gmail.com>
2025-01-19 21:23:46 +0100

    new config for env

--- src/config/config.js
+++ src/config/config.js
@@ -1,12 +1,12 @@
 const config = {
     github: {
-      url: import.meta.env.VITE_GITHUB_URL || 'https://github.com'
+      url:process.env.VITE_GITHUB_URL || import.meta.env.VITE_GITHUB_URL || 'https://github.com'
     },
     coffee: {
-      url: import.meta.env.VITE_COFFEE_URL || 'https://buymeacoffee.com'
+      url: process.env.VITE_COFFEE_URL || import.meta.env.VITE_COFFEE_URL || 'https://buymeacoffee.com'
     },
     app: {
-      title: import.meta.env.VITE_APP_TITLE || 'Download My Memories'
+      title: process.env.VITE_APP_TITLE || import.meta.env.VITE_APP_TITLE || 'Download My Memories'
     }
   };
   
